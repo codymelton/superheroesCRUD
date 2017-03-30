5d4d75ab08ce27de470c498a4c5dd6c217b6d71b
Cody Melton <cgm@Codys-MacBook-Air.local>
2017-03-29 20:48:35 -0600

    Added database and mongoose connection

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ had to run npm init to make a package.json => every node proj has one of these
 had to install express so we can use it as our dev server
 
 create an endpoint /test to handle a GET request...
+
+
+We defined a mongoose schema in the file models/superhero
+A schema is like a blueprint, it defines what all future heroes will
+look like.  It will be a constructor function we will use to actually
+make new superheroes.
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
+var Superhero = require('./models/superhero')
 var app     = express();
+
+var mongoose = require('mongoose');
+
+//required to connect to our local database.
+//it will look for/ or create a db called superheroes.
+mongoose.connect("mongodb://localhost/superheroes");
+
 
 // app.METHOD('URL LOCATION', function(req, res))
 
@@ -8,6 +16,18 @@ app.get('/test', function (req, res) {
   res.send("You found the test route.");
 })
 
+var superHero1 = {
+  name: "Superman",
+  superPower: "I can fly!",
+  universe: "DC",
+  evil: false,
+  rank: 10
+};
+
+app.get('/superHero', function(req, res){
+  res.json(superHero1);
+});
+
 var server = app.listen(3000, function(){
-  console.log("Server is 👾 on PORT 3000");
+  console.log("Server is 💩  on PORT 3000");
 });
