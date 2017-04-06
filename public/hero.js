e310f35c008483cc162a4db277c01393f2ac8103
Cody Melton <cgm@Codys-MacBook-Air.local>
2017-04-05 21:29:25 -0600

    Added CSS and frontpage work

--- public/hero.js
+++ public/hero.js
@@ -1,12 +1,13 @@
 // test to ensure that the js files were linked.
 // console.log("Hello from hero.js inside of public directory");
 
-var sampleVue = new Vue({
-  el: "#sample",
+var componentVue = new Vue({
+  el: "#component",
   data: {
     numbers: [1,2,3,4,5,6,7,8,9],
     title: "Hello from sample",
-    heroes: []
+    heroes: [],
+    hero: {}
   },
 });
 
@@ -16,5 +17,6 @@ fetch("/api/superheroes")  //Gets the information from json
   })
   .then(function(data){
     console.table(data);
-    sampleVue.heroes = data;
+    componentVue.heroes = data;
+    componentVue.hero = data[1];
   })
