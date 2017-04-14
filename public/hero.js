209f1f572cca7b3c5f8355b9b5536bd94ea34e12
Cody Melton <cgm@Codys-MacBook-Air.local>
2017-04-13 21:43:06 -0600

    Added delete and post button to project

--- public/hero.js
+++ public/hero.js
@@ -2,13 +2,41 @@
 // console.log("Hello from hero.js inside of public directory");
 
 var componentVue = new Vue({
-  el: "#component",
+  el: "#heropannel",
   data: {
-    numbers: [1,2,3,4,5,6,7,8,9],
-    title: "Hello from sample",
     heroes: [],
-    hero: {}
+    hero: {},
+    newName: "",//*****MATCH TO SUPERHERO.JS SCHEMA and line 28
+    newSuperpower: "",
+    newEvil: "",
   },
+  methods: {
+    deleteHero: function(id){
+      if (confirm("Do you want to delete?")) {
+        $.ajax({
+          url: "/api/superheroes/"+id,
+          method: "DELETE"
+        }).done(function(data){
+          window.location = "/heroes"
+        })
+      }
+    },
+    submitHero: function(e) {
+      e.preventDefault()
+      alert("I was found")
+      var data = {
+        name: this.newName, //GET THIS ALL SET UP
+        superPowers: this.newSuperpower,
+      }
+      $.ajax({
+        url: "/api/superheroes",
+        method: "POST",
+        data: data
+      }).done(function(res){
+        console.log(res);
+      })
+    }
+  }
 });
 
 fetch("/api/superheroes")  //Gets the information from json
@@ -17,6 +45,6 @@ fetch("/api/superheroes")  //Gets the information from json
   })
   .then(function(data){
     console.table(data);
-    componentVue.heroes = data;
+    componentVue.heroes = data.data;
     componentVue.hero = data[1];
   })
